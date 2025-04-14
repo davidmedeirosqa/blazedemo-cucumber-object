5be63a3fddb7fd988e376f4160bcec949c7872aa
David Medeiros <davidmedeiros94@hotmail.com>
2025-04-14 18:26:32 -0300

    renomeado arquivo BasePage por Base e estruturado Base e HomePage

---
+++ src/main/java/pages/Base.java
@@ -0,0 +1,8 @@
+package pages; // Define o pacote onde a classe está localizada
+
+import org.openqa.selenium.WebDriver; // Importa o WebDriver para controle do navegador
+
+public class Base { // Classe simples que pode servir de base para outras, se for estendida
+
+    public WebDriver driver; // Declaração do WebDriver (obs: nome ideal seria "driver" para manter padrão)
+}
--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,5 +1,23 @@
-package pages;
+package pages; // Define o pacote onde a classe está localizada
 
-public class HomePage {
-    
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver; // Importa o WebDriver para controle do navegador
+import org.openqa.selenium.support.PageFactory; // Importa o PageFactory para inicializar os elementos da página
+
+// HomePage herda de CommonPage, ou seja, ela tem acesso aos atributos e métodos de CommonPage
+public class HomePage extends CommonPage { 
+
+    // Construtor - Inicializa a classe HomePage e os elementos da página com o WebDriver
+    public HomePage(WebDriver driver) {
+        super(driver); // Chama o construtor da classe pai (CommonPage) passando o driver
+        PageFactory.initElements(driver, this); // Inicializa os elementos Web da página usando o PageFactory
+    }
+
+    // Elementos mapeados
+    public By byLocal(String local){
+        return By.cssSelector("option[value=\"" + local + "\"]");
+    }
+
+
+    // Ações com os elementos mapeados
 }
